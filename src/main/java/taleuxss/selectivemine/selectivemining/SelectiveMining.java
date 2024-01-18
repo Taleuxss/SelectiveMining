61f597416486e127dbfc503b581a8408947e68e4
Taleuxss <50016554+Taleuxss@users.noreply.github.com>
2024-01-17 18:07:05 -0800

    Delete src/main directory

--- src/main/java/taleuxss/selectivemine/selectivemining/SelectiveMining.java
+++
@@ -1,8 +0,0 @@
-package taleuxss.selectivemine.selectivemining;
-
-import net.fabricmc.api.ModInitializer;
-
-public class SelectiveMining implements ModInitializer {
-    public void onInitialize() {
-    }
-}
